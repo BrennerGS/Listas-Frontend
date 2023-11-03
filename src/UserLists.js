4f0c38f6b27e2c9fdde88f1d3cfc8e0a9b20e177
BrennerGS <38071197+BrennerGS@users.noreply.github.com>
2023-11-03 10:06:10 -0300

    Criação de loginComponent e form

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ListComponent from "./ListComponent";
+import LoginComponent from "./LoginComponent";
 
 export default class UserLists extends React.Component{
     state = { lists:[] , loading: false}
@@ -9,9 +10,9 @@ export default class UserLists extends React.Component{
                 'Content-Type': 'application/json'
             }
         }
-        config.headers['Authorization'] = "Token bd14cf36b74e7d8e6b5119f8a8e20f741c870a3d";
+        config.headers['Authorization'] = "Token ffbcb7be9ab960f6fe2635185b40e5c2264c1109";
 
-        var url = 'http://127.0.0.1:8000/list/';
+        var url = 'http://127.0.0.1:8000/item/';
         const response = await fetch(url, config);
         const data = await response.json();
         console.log(data.results);
@@ -20,10 +21,14 @@ export default class UserLists extends React.Component{
     render()
     {
         const listsApi = this.state.lists;
-        return (
-            <div>
-                {listsApi.map(list => <ListComponent key={list.id} listName={list.name} items={list.item_set} />)}
-            </div>
-        )
+        var token = '';
+        if(token==='')
+           return <LoginComponent/>
+        else
+            return (
+                <div>
+                    {listsApi.map(list => <ListComponent key={list.id} listName={list.name} items={list.item_set} />)}
+                </div>
+            )
     }
 }
